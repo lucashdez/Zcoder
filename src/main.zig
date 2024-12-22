04d5e5e44e4bcdbb2a453cef3fce77676fc8c47f
lucashdez <lukastpiano@gmail.com>
2024-12-22 10:47:04 +0000

    Added window and renderer
    way to render text: let's goo

--- src/main.zig
+++ src/main.zig
@@ -1,33 +1,36 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
 const std = @import("std");
+const sdl = @cImport({
+    @cInclude("SDL2/SDL.h");
+});
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    if (sdl.SDL_Init(sdl.SDL_INIT_VIDEO) < 0) {
+        return error.MainSDLError;
+    }
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    _ = sdl;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var window: *sdl.SDL_Window = undefined;
+    //var surface: *sdl.SDL_Surface = undefined;
+    var renderer: *sdl.SDL_Renderer = undefined;
 
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-    const input_bytes = std.testing.fuzzInput(.{});
-    try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input_bytes));
+    window = sdl.SDL_CreateWindow("my window", 100, 100, 640, 480, sdl.SDL_WINDOW_SHOWN | sdl.SDL_WINDOW_RESIZABLE).?;
+    renderer = sdl.SDL_CreateRenderer(window, -1, sdl.SDL_RENDERER_ACCELERATED).?;
+    //surface = sdl.SDL_GetWindowSurface(window);
+    var quit: bool = false;
+    while (!quit) {
+        var event: sdl.SDL_Event = undefined;
+        while (sdl.SDL_PollEvent(&event) > 0) {
+            switch (event.type) {
+                sdl.SDL_QUIT => {
+                    quit = true;
+                },
+                else => {},
+            }
+        }
+        _ = sdl.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 0);
+        _ = sdl.SDL_RenderClear(renderer);
+        sdl.SDL_RenderPresent(renderer);
+    }
+    sdl.SDL_Quit();
 }
