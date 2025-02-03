e8d0c940f291dda7b25a7204355055a4d3e4a053
lucashdez <lukastpiano@gmail.com>
2025-02-03 10:28:07 +0000

    To MSI

--- src/graphics/wayland.zig
+++ src/graphics/wayland.zig
@@ -3,6 +3,8 @@ const sdl = @cImport(@cInclude("SDL2/SDL.h"));
 const u = @import("lhvk_utils.zig");
 const raw = @import("../os/wayland/wayland.zig");
 const assert = std.debug.assert;
+const e = @import("windowing/events.zig");
+const lhmem = @import("../memory/memory.zig");
 
 fn glb_reg_handler(data: ?*anyopaque, registry: ?*raw.wl_registry, name: u32, interface: [*c]const u8, version: u32)
 callconv(.C) void
@@ -13,7 +15,10 @@ callconv(.C) void
         const len = std.mem.len(interface);
         if (std.mem.eql(u8, interface[0..len], "wl_compositor"))
         {
-            reg_data.compositor = @ptrCast(@alignCast(raw.wl_registry_bind(registry, name, &raw.wl_compositor_interface, version)));
+            const proxy = raw.wl_registry_bind(registry, name, &raw.wl_compositor_interface, version);
+            assert(proxy != null);
+            _ = raw.wl_proxy_set_queue(@as(*raw.wl_proxy, @ptrCast(proxy)), reg_data.queue);
+            reg_data.compositor = @ptrCast(@alignCast(proxy));
         }
     }
 
@@ -27,13 +32,15 @@ callconv(.C) void {
     _ = name;
 }
 
-
+// TODO: Add queue and regglobalhanddler handle queue things.
 pub const WaylandProps = struct {
     display: ?*raw.wl_display,
     surface: ?*raw.wl_surface,
     compositor: ?*raw.wl_compositor,
+    compositor_proxy: ?*raw.wl_proxy,
     registry: ?*raw.wl_registry,
     listener: ?raw.wl_registry_listener,
+    queue: ?*raw.wl_event_queue,
     //global: ?*const fn (?*anyopaque, ?*struct_wl_registry, u32, [*c]const u8, u32) callconv(.C) void = @import("std").mem.zeroes(?*const fn (?*anyopaque, ?*struct_wl_registry, u32, [*c]const u8, u32) callconv(.C) void),
     //global_remove: ?*const fn (?*anyopaque, ?*struct_wl_registry, u32) callconv(.C) void = @import("std").mem.zeroes(?*const fn (?*anyopaque, ?*struct_wl_registry, u32) callconv(.C) void),
 };
@@ -43,6 +50,10 @@ pub const Window = struct {
     raw: WaylandProps,
     width: u32,
     height: u32,
+    events: e.EventList,
+    pub fn get_events(window: *Window) void {
+        _ = window;
+    }
 };
 
 
@@ -52,22 +63,36 @@ pub fn create_window(name: []const u8) Window {
 
     props.display = raw.wl_display_connect(null);
     assert(props.display != null);
+
+    props.queue = raw.wl_display_create_queue(props.display.?);
+    assert(props.queue != null);
+
     props.registry = raw.wl_display_get_registry(props.display.?);
     assert(props.registry != null);
+
+    _ = raw.wl_proxy_set_queue(@as(*raw.wl_proxy, @ptrCast(props.registry)), props.queue);
+
     props.listener =  raw.wl_registry_listener {
         .global = glb_reg_handler,
         .global_remove = glb_reg_remover,
     };
     _ = raw.wl_registry_add_listener(props.registry.?, &props.listener.?, &props);
-    _ = raw.wl_display_roundtrip(props.display.?);
+    assert(props.compositor != null);
+    props.surface = raw.wl_compositor_create_surface(props.compositor.?);
+    assert(props.surface != null);
 
+    raw.wl_proxy_set_queue(@ptrCast(props.registry.?), props.queue);
+    raw.wl_proxy_set_queue(@ptrCast(props.compositor.?), props.queue);
+    raw.wl_proxy_set_queue(@ptrCast(props.surface.?), props.queue);
 
+    _ = raw.wl_display_roundtrip_queue(props.display.?, props.queue);
 
 
 
     return Window {
     .handle = null,
     .raw = props,
+    .events = e.EventList{.first = null, .last = null, .arena = lhmem.make_arena(1 << 10)},
     .width = 800,
     .height = 600,
     };
