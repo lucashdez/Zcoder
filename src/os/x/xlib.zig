023fec2efcb79f8088485c2eb867afcb4a6c01cd
lucashdez <lukastpiano@gmail.com>
2025-02-16 17:02:15 +0000

    To HP

--- src/os/x/xlib.zig
+++ src/os/x/xlib.zig
@@ -1,3 +1,11 @@
-pub const Display = opaque {}; 
+pub const Display = opaque {};
+pub const Window = opaque {};
+pub const Visual = opaque {};
+
+pub const XSetWindowAttributes = struct {
+    bit_gravity: i32,
+};
 
 pub extern fn XOpenDisplay(name: [:0]u8) callconv(.c) ?*Display;
+
+pub extern fn XCreateWindow(display: ?*Display, parent: ?*Window, x: i32, y: i32, width: u32, height: u32, border_width: u32, depth: i32, class: u32, visual: ?*Visual, valuemask: u64, attributes: ?*XSetWindowAttributes) ?*Window;
